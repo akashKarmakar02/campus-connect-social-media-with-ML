a51d19809d43c755d4a3d4bb0e5ed4c2ab9457bc
akashKarmakar02 <akash.anan03@gmail.com>
2023-08-02 07:27:20 +0530

    included post urls to global urls

--- campus_connect_backend/campus_connect_backend/urls.py
+++ campus_connect_backend/campus_connect_backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-
+    path('api/post/', include('post.urls'))
 ]
